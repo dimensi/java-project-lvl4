40d6939c820b27fc98ab4b82062b1cc00646bda7
Nikita Nafranets <root@dimensi.dev>
2022-08-05 03:13:07 +0300

    feat: add template engine

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,11 +1,16 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.valueOf(port);
+        return Integer.parseInt(port);
     }
 
     private static String getMode() {
@@ -16,12 +21,35 @@ public class App {
         return getMode().equals("production");
     }
 
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+    public static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.render("index.html"));
+    }
+
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.enableDevLogging();
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
         });
-        app.get("/", ctx -> ctx.result("Hello world! 2"));
 
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        addRoutes(app);
         return app;
     }
 
